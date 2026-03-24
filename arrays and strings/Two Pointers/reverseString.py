5760296080b819d265dee25afd1980ce4778d4d6
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2026-03-24 16:14:13 -0600

    Two Pointers technique
    The two pointers technique is a common algorithmic strategy used in solving array, string, and linked list problems efficiently. It involves using two pointers (or indices) to traverse data structures, often to reduce complexity from O(n²) to O(n).
    Key Concepts
    Pointers: Typically, one pointer (left) starts at the beginning and another (right) starts either at the end or follows some specific logic.
    Movement: Pointers move based on conditions, narrowing down ranges or pairing elements efficiently.
    Use Cases: Problems involving arrays or strings where comparisons, sums, reversals, or partitions are needed.

---
+++ arrays and strings/Two Pointers/reverseString.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/reverse-string/description/
+
+def reverseString(s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return s
